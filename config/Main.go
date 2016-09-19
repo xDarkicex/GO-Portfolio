6eb146c529167c50166f6f28eedaad12f07e6a38
Gentry Rolofson <xdarkicex@gmail.com>
2016-09-18 23:10:21 -0700

    init commit GO-Portfolio

---
+++ config/Main.go
@@ -0,0 +1,11 @@
+package main
+  import(
+
+  )
+  func init(){
+
+  }
+  func main(){
+
+
+}
---
+++ config/Routes.go
@@ -0,0 +1,24 @@
+// Go routes based on julien schmidt http router
+// besic setup
+package routes
+  import(
+    "fmt"
+    "github.com/julienschmidt/httprouter"
+    "net/http"
+    "log"
+  )
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+    fmt.Fprint(w, "Welcome!\n")
+}
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    fmt.Fprintf(w, "hello, %s!\n", ps.ByName("name"))
+}
+
+func main() {
+    router := httprouter.New()
+    router.GET("/", Index)
+    // router.GET("/hello/:name", Hello)
+
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
