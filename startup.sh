cd7210320446af3a0edf888387940da98015b541
Gentry Rolofson <xdarkicex@gmail.com>
2016-12-05 14:16:50 -0800

    Bull Shit Commit

--- startup.sh
+++ startup.sh
@@ -4,15 +4,15 @@ export SMTPHOST=smtp.gmail.com
 export SMTPPORT=587
 export SMTPPASSWORD='Vh402152Go!'
 export ENV=production
-sass --watch ./app/assets/stylesheets/:./public/assets/stylesheets/ --style compressed &
+/home/ubuntu/.rvm/gems/ruby-2.3.0/bin/sass --watch ./app/assets/stylesheets/:./public/assets/stylesheets/ --style compressed &
 PID=$!
 sleep 5
 kill $PID
 
-tsc --outDir ./public/assets/scripts/application/ ./app/assets/typescripts/application/*.ts
-tsc --outDir ./public/assets/scripts/users/ ./app/assets/typescripts/users/*.ts
-tsc --outDir ./public/assets/scripts/blog/ ./app/assets/typescripts/blog/*.ts
-tsc --outDir ./public/assets/scripts/examples/ ./app/assets/typescripts/examples/*.ts
-tsc --outDir ./public/assets/scripts/ ./app/assets/typescripts/*.ts
+/usr/local/bin/tsc --outDir ./public/assets/scripts/application/ ./app/assets/typescripts/application/*.ts
+/usr/local/bin/tsc --outDir ./public/assets/scripts/users/ ./app/assets/typescripts/users/*.ts
+/usr/local/bin/tsc--outDir ./public/assets/scripts/blog/ ./app/assets/typescripts/blog/*.ts
+/usr/local/bin/tsc --outDir ./public/assets/scripts/examples/ ./app/assets/typescripts/examples/*.ts
+/usr/local/bin/tsc --outDir ./public/assets/scripts/ ./app/assets/typescripts/*.ts
 
 /home/ubuntu/bin/PortfolioGo
