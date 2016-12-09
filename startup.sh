017c5f3d0acdf95638b10617e99621edc7f466ae
Gentry Rolofson <xdarkicex@gmail.com>
2016-12-08 17:25:50 -0800

    text-align

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 /home/ubuntu/.rvm/gems/ruby-2.3.0/bin/sass --watch ./app/assets/stylesheets/:./public/assets/stylesheets/ --style compressed &
-PID=$!
-sleep 5
-kill $PID
+# PID=$!
+# sleep 5
+# kill $PID
 
 /usr/local/bin/tsc --outDir ./public/assets/scripts/application/ ./app/assets/typescripts/application/*.ts
 /usr/local/bin/tsc --outDir ./public/assets/scripts/users/ ./app/assets/typescripts/users/*.ts
