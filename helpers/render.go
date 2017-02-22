95a58a2467a34c3b24692aa7b321c188d223f8f6
Gentry Rolofson <xdarkicex@gmail.com>
2017-02-22 01:44:25 -0800

    add keep alive headers

--- helpers/render.go
+++ helpers/render.go
@@ -21,6 +21,9 @@ var t *template.Template
 
 //Render function renders page with our data
 func Render(a RouterArgs, view string, object map[string]interface{}) {
+	a.Response.Header().Set("Connection", "Keep-Alive")
+	a.Response.Header().Set("Vary", "Accept-Encoding")
+	a.Response.Header().Set("Cache-Control", "private, max-age=7776000")
 	times := make(map[string]interface{})
 	times["total"] = time.Now()
 
