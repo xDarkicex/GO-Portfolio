9215bbf16a2a6e96ccb42b3ff7efc37bc161a566
Gentry Rolofson <0509479@my.scccd.edu>
2023-12-12 17:25:53 -0800

    Update cached.go
    Changed cache code to add stability, protection for race condition and extends code to accept configuration.

--- helpers/cached.go
+++ helpers/cached.go
@@ -5,6 +5,92 @@ import (
 	"time"
 )
 
+// CacheObject represents a cached object with expiration time.
+type CacheObject struct {
+	Expire time.Time
+	Object interface{}
+}
+
+// Cache defines the contract for a cache.
+type Cache interface {
+	Get(key string, fallback func() *CacheObject) *CacheObject
+	Delete(key string)
+	SetCleanupInterval(interval time.Duration)
+}
+
+// InMemoryCache is a simple in-memory cache with expiration.
+type InMemoryCache struct {
+	cache             map[string]*CacheObject
+	mutex             sync.RWMutex
+	defaultExpiration time.Duration
+	cleanupInterval   time.Duration
+}
+
+// NewInMemoryCache initializes a new in-memory cache with the given default expiration duration and cleanup interval.
+func NewInMemoryCache(defaultExpiration, cleanupInterval time.Duration) *InMemoryCache {
+	cache := &InMemoryCache{
+		cache:             make(map[string]*CacheObject),
+		defaultExpiration: defaultExpiration,
+		cleanupInterval:   cleanupInterval,
+	}
+	go cache.periodicCleanup()
+	return cache
+}
+
+// Get retrieves a cached object by key. If not found, it invokes the fallback function to compute the object.
+func (c *InMemoryCache) Get(key string, fallback func() *CacheObject) *CacheObject {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	if obj, ok := c.cache[key]; ok {
+		return obj
+	}
+
+	newObject := fallback()
+	c.cache[key] = newObject
+	return newObject
+}
+
+// Delete removes a cached object by key.
+func (c *InMemoryCache) Delete(key string) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	delete(c.cache, key)
+}
+
+// SetCleanupInterval sets a new cleanup interval for the cache.
+func (c *InMemoryCache) SetCleanupInterval(interval time.Duration) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.cleanupInterval = interval
+}
+
+// periodicCleanup periodically purges expired items from the cache.
+func (c *InMemoryCache) periodicCleanup() {
+	ticker := time.NewTicker(c.cleanupInterval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		c.mutex.Lock()
+		for key, cachedObject := range c.cache {
+			if time.Now().After(cachedObject.Expire) {
+				delete(c.cache, key)
+			}
+		}
+		c.mutex.Unlock()
+	}
+}
+
+
+/* 
+// original code
+package helpers
+
+import (
+	"sync"
+	"time"
+)
+
 // CacheObject shut up
 type CacheObject struct {
 	Expire time.Time
@@ -63,3 +149,4 @@ func init() {
 	})
 
 }
+*/
