cb2b34eabc4e86b44a89dda411c61cfeae6fff3c
Gentry Rolofson <0509479@my.scccd.edu>
2023-12-12 17:33:57 -0800

    Update pidCapture.go
    Adds Error handling and safely closes file

--- helpers/pidCapture.go
+++ helpers/pidCapture.go
@@ -6,9 +6,26 @@ import (
 	"syscall"
 )
 
-// Pidder function captures the programs active pi for later use inside AWS, very usefull to know programs pid ID
-func Pidder() {
+const (
+	pidFileName      = "pid.txt"
+	filePermission   = os.O_RDWR | os.O_CREATE
+	filePermissionRW = 0666
+)
+
+// Pidder captures the program's active PID and writes it to a file.
+func Pidder() error {
 	SPID := syscall.Getpid()
-	file, _ := os.OpenFile("pid.txt", os.O_RDWR|os.O_CREATE, 0666)
-	file.WriteString(fmt.Sprintf("%d", SPID))
+
+	file, err := os.OpenFile(pidFileName, filePermission, filePermissionRW)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer Close(file) // Close is a helper function to safely close an io.Closer
+
+	_, err = file.WriteString(fmt.Sprintf("%d", SPID))
+	if err != nil {
+		return fmt.Errorf("failed to write PID to file: %w", err)
+	}
+
+	return nil
 }
